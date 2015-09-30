9534d409ae9b11df28dfa4e74c5b8acb3ccc7f50
Ryan Goodfellow <rgoodfel@isi.edu>
2015-09-30 12:14:07 -0700

    talking to oven

--- pizzachef.rb
+++ pizzachef.rb
@@ -1,6 +1,8 @@
 #!/usr/bin/env ruby
 
 require 'webrick'
+require 'net/http'
+require 'uri'
 
 server = WEBrick::HTTPServer.new :Port => 8081
 
@@ -9,7 +11,13 @@ trap 'INT' do server.shutdown end
 server.mount_proc '/pieRequest' do |req, res|
   cnt = req.query()['count']
   puts "%d pies requested" % cnt
-  sleep(4.7)
+  sleep(1)
+  uri = URI.parse("http://woodbrick.deterlab.net:8082")
+  http = Net::HTTP.new(uri.host, uri.port)
+  request = Net::HTTP::Get.new("/cook")
+  response = http.request(request)
+  puts "The oven says %s" % response.body
+  sleep(1)
   res.body = cnt
 end
 
